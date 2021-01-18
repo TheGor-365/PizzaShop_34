b648eb68c5f610e2bacd56a3c4da0ca67f0d85aa
Gor <7495500@gmail.com>
2021-01-18 20:49:10 +0300

    Add cart_get_number_of_items function

--- public/script.js
+++ public/script.js
@@ -10,4 +10,19 @@ function add_to_cart(id) {
   let x = window.localStorage.getItem(key)
   x++
   window.localStorage.setItem(key, x)
+  alert('Items in your cart: ' + cart_get_number_of_items())
+}
+
+function cart_get_number_of_items() {
+  let cnt = 0
+
+  for(let i = 0; i < window.localStorage.length; i++) {
+    let key = window.localStorage.key(i)
+    let value = window.localStorage.getItem(key)
+
+    if (key.indexOf('product_') === 0) {
+      cnt += value * 1
+    }
+  }
+  return cnt
 }
